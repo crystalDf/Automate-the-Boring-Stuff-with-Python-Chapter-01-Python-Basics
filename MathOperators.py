c0c2ec7acc1a80613b25710e8bd83b380c86c343
crystalDf <chendong333@gmail.com>
2016-03-14 23:36:41 +0800

    Chapter 01 Python Basics

---
+++ MathOperators.py
@@ -0,0 +1,12 @@
+# ** Exponent
+print(2 ** 3)
+
+# % Modulus/remainder
+print(22 % 8)
+
+# // Integer division/ oored quotient
+print(22 // 8)
+
+# when the * operator is used on one string value and one integer value,
+# it becomes the string replication operator.
+print('Alice' * 5)
